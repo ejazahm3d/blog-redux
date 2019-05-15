8fb593cfb490d334320248262b0c7fbd53d8f354
Ejaz Ahmed <ejazahmed.dev@gmail.com>
2019-05-15 10:56:09 +0500

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import PostList from "./PostList";
 
 const App = () => {
-  return <div className="ui container">Hello World</div>;
+  return (
+    <div className="ui container">
+      <PostList />
+    </div>
+  );
 };
 
 export default App;
